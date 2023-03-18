62082322337fb20f937ed43bcbcc3d68fa2e3897
Dowon Yang <dowon.yang.us@gmail.com>
2023-03-18 18:40:29 -0400

    fcfs done

--- 4_disk_scheduling/disk.c
+++ 4_disk_scheduling/disk.c
@@ -1,8 +1,58 @@
 #include "oslabs.h"
 
-struct RCB handle_request_arrival_fcfs(struct RCB request_queue[QUEUEMAX], int *queue_cnt, struct RCB current_request, struct RCB new_request, int timestamp);
-struct RCB handle_request_completion_fcfs(struct RCB request_queue[QUEUEMAX],int *queue_cnt); 
-struct RCB handle_request_arrival_sstf(struct RCB request_queue[QUEUEMAX],int *queue_cnt, struct RCB current_request, struct RCB new_request, int timestamp); 
-struct RCB handle_request_completion_sstf(struct RCB request_queue[QUEUEMAX],int *queue_cnt,int current_cylinder); 
-struct RCB handle_request_arrival_look(struct RCB request_queue[QUEUEMAX],int *queue_cnt, struct RCB current_request, struct RCB new_request, int timestamp);
-struct RCB handle_request_completion_look(struct RCB request_queue[QUEUEMAX],int *queue_cnt, int current_cylinder, int scan_direction);
+struct RCB NULLRCB = {
+        .request_id = 0,
+        .arrival_timestamp = 0,
+        .cylinder = 0,
+        .address = 0,
+        .process_id = 0
+};
+
+int _is_null_RCB(struct RCB block) {
+    if (block.request_id == 0 &&
+        block.arrival_timestamp == 0 &&
+        block.cylinder == 0 &&
+        block.address == 0 &&
+        block.process_id == 0
+    ) { return 1; }
+    return 0;
+}
+
+void _remove_from_RCB_queue(struct RCB request_queue[QUEUEMAX],int *queue_cnt, int target_index) {
+    for(int i=target_index; i<*queue_cnt; i++) {
+        request_queue[i] = request_queue[i+1];
+    }
+    (*queue_cnt)--;
+}
+
+struct RCB handle_request_arrival_fcfs(struct RCB request_queue[QUEUEMAX], int *queue_cnt, struct RCB current_request, struct RCB new_request, int timestamp) {
+    if(_is_null_RCB(current_request)) { return new_request; }
+    request_queue[(*queue_cnt)++] = new_request;
+    return current_request;
+}
+
+struct RCB handle_request_completion_fcfs(struct RCB request_queue[QUEUEMAX],int *queue_cnt) {
+    if (*queue_cnt == 0) { return NULLRCB; }
+    int earliest_arrival_index = 0;
+    for (int i=1; i<*queue_cnt; i++) {
+        if (request_queue[i].arrival_timestamp < request_queue[earliest_arrival_index].arrival_timestamp) {
+            earliest_arrival_index = i;
+        }
+    }
+    struct RCB next_RCB = request_queue[earliest_arrival_index];
+    _remove_from_RCB_queue(request_queue, queue_cnt, earliest_arrival_index);
+    return next_RCB;
+} 
+
+struct RCB handle_request_arrival_sstf(struct RCB request_queue[QUEUEMAX],int *queue_cnt, struct RCB current_request, struct RCB new_request, int timestamp) {
+    return NULLRCB;
+}
+struct RCB handle_request_completion_sstf(struct RCB request_queue[QUEUEMAX],int *queue_cnt,int current_cylinder) {
+    return NULLRCB;
+}
+struct RCB handle_request_arrival_look(struct RCB request_queue[QUEUEMAX],int *queue_cnt, struct RCB current_request, struct RCB new_request, int timestamp) {
+    return NULLRCB;
+}
+struct RCB handle_request_completion_look(struct RCB request_queue[QUEUEMAX],int *queue_cnt, int current_cylinder, int scan_direction) {
+    return NULLRCB;
+}
